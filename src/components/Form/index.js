0b031c222b6aa9fa0149dbb9018a0795ab431542
Philip Hurst <youngphil9@gmail.com>
2020-05-23 11:49:37 -0500

    added jumbotron and form components

---
+++ src/components/Form/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Form() {
+    return (
+        <form>
+            <div class="form-group">
+                <label for="exampleFormControlSelect1">Example select</label>
+                <select class="form-control" id="exampleFormControlSelect1">
+                    <option>1</option>
+                </select>
+            </div>
+        </form>
+    );
+}
+
+export default Form;
---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron() {
+    return (
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">Employee Directory</h1>
+                <p class="lead">Filter or sort employees by provided categories.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Jumbotron;
